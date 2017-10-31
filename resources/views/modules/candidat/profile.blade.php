82c6ba3748944c2609c95eedb51378b790ad06ba
farestayari <fares-1993@hotmail.fr>
2017-10-31 17:02:48 +0100

    front end fix

--- resources/views/modules/candidat/profile.blade.php
+++ resources/views/modules/candidat/profile.blade.php
@@ -1,63 +1,58 @@
 @extends('layouts.app')
 @section('content')
   <div class="container">
-      <div class="row">
-          <div class="col-md-8 col-md-offset-2">
+
+
+          <div class="col-md-4">
+            <div class="panel panel-default">
+              <div class="panel-heading">
+                {{$candidats->nom}} {{$candidats->prenom}}
+              </div>
+              <div class="panel-body">
+                CIN : {{$candidats->cin}}
+                <br>
+                telephone : {{$candidats->telephone}}
+                <br>
+                Adresse : {{$candidats->adress}}
+                <br>
+                Nationalité: {{$candidats->nationalité}}
+                <br>
+                Categorie : {{$candidats->categorie}}
+                <br>
+                Type :  {{$candidats->type}}
+                <br>
+              Date de naissance : {{$candidats->date_naissance}}
+                <br>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-8 ">
               <div class="panel panel-default">
-                  <div class="panel-heading">Mes personnels
-                                      
-
-
+                  <div class="panel-heading">
+                    Mes personnels
                   </div>
                   <div class="panel-body">
-                      <table class="table">
-          <thead>
-              <tr>
-                  <th>prenom</th>
-                  <th>nom</th>
-                  <th>adresse</th>
-                  <th>Cin</th>
-                  <th>telephone</th>
-                  <th>Action</th>
-              </tr>
-          </thead>
-          <tfoot>
-              <tr>
-                  <th>prenom</th>
-                  <th>nom</th>
-                  <th>adress</th>
-                  <th>cin</th>
-                  <th>telephone</th>
-                  <th>Action</th>
-
-              </tr>
-          </tfoot>
-
-          @if($candidats)
-         <tbody>
-             <tr>
-                 <td>{{$candidats->nom}}</td>
-                 <td>{{$candidats->prenom}}</td>
-                 <td>{{$candidats->cin}}</td>
-                 <td>{{$candidats->telephone}}</td>
-                 <td>{{$candidats->adress}}</td>
-                 <td>{{$candidats->nationalité}}</td>
-                 <td>{{$candidats->categorie}}</td>
-                 <td>{{$candidats->type}}</td>
-                 <td>{{$candidats->date_naissance}}</td>
-                 <th><a class="btn btn-info fa fa-user" href="#"></a>
-                 <a class="btn btn-info fa fa-trash" href="#"></a>
-               </th>
-             </tr>
-         </tbody>
-         @endif
-
-      </table>
+                      test
                   </div>
               </div>
           </div>
-      </div>
+
+
+
   </div>
+
+  {{-- <div class="container">
+    <div class="col-md-4">
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          test
+        </div>
+        <div class="panel-body">
+          another test
+        </div>
+      </div>
+    </div>
+  </div> --}}
 
 
 @endsection
